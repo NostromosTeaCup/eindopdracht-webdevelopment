604704ded712ac9f409370514b44e37005d3392a
NostromosTeaCup <lvanmierlo@protonmail.com>
2022-09-14 12:48:05 +0200

    Recipe form/html updated, API fetch and creating recipe cards are made. general CSS styling on the cards done.

---
+++ src/functions/fetchRecipeData.js
@@ -0,0 +1,91 @@
+// //imports
+// import axios from "axios"
+// import createRecipeCard from "./createRecipeCard"
+//
+// //Fetching data from API
+// export default async function fetchRecipeData(searchQuery, mealType, cuisine, diet, time) {
+//
+//     //Fetching is succesfull
+//     try {
+//
+//         //declare unput values for API
+//         const URI = "https://api.edamam.com";
+//         const ENDPOINT = "/api/recipes/v2";
+//         const API_ID = "f4692aaf";
+//         const API_KEY = "36946acd92c05fd67adcdc44b3dfdf0e";
+//
+//         //fetch data from api
+//         const response = await axios.get( URI + ENDPOINT, {
+//             params: {
+//                 type: "public",
+//                 app_id: API_ID,
+//                 app_key: API_KEY,
+//                 q: searchQuery,
+//                 mealType: mealType,
+//                 cuisine: cuisine,
+//                 diet: diet,
+//                 time: time
+//             }
+//         })
+//
+//         //store recipe key in variable
+//         const arrayOfRecipes = response.data.hits
+//         // console.log(arrayOfRecipes)
+//
+//         createRecipeCard(arrayOfRecipes);
+//
+//         //catch error message and show them in UI
+//     } catch (e) {
+//         const error = document.getElementById('error-message')
+//
+//         if (e.response.status === 404) {
+//             error.innerContent = 'page not found'
+//         } else if (e.response.status === 500) {
+//             error.innerContent = 'internal server error'
+//         }
+//     }
+// }
+
+import axios from "axios";
+import createRecipeCard from "./createRecipeCard";
+
+// Fetching data from edamam API
+export default async function fetchRecipeData(searchQuery, mealType) {
+
+    //if fetching is succesfull
+    try {
+
+        //declare input values for API
+        const URI = "https://api.edamam.com";
+        const ENDPOINT = "/api/recipes/v2";
+        const API_ID = "f4692aaf";
+        const API_KEY = "36946acd92c05fd67adcdc44b3dfdf0e";
+
+        //fetch data from api
+        const response = await axios.get(URI + ENDPOINT, {
+            params: {
+                type: "public",
+                app_id: API_ID,
+                app_key: API_KEY,
+                q: searchQuery,
+                mealType: mealType
+            }
+        })
+
+        //store recipe key in variable
+        const arrayOfRecipes = response.data.hits
+        // console.log(arrayOfRecipes);
+
+        createRecipeCard(arrayOfRecipes);
+
+        //catch error message and show them in UI
+    } catch (e) {
+        const error = document.getElementById("error-message")
+
+        if (e.response.status === 404) {
+            error.innerContent = 'page not found';
+        } else if (e.response.status === 500) {
+            error.innerContent = 'internal server error';
+        }
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,56 @@
-console.log("Hello World!")
+// //imports
+// import fetchRecipeData from "./functions/fetchRecipeData";
+//
+// //reference to form submit
+// const submitForm = document.getElementById('onSubmit');
+//
+// //reference to input fields
+// const ingredients = document.getElementById('searchQuery');
+// const mealType = document.getElementById('mealType');
+// const cuisine = document.getElementById('cuisine');
+// const diet = document.getElementById('diet');
+// const time = document.getElementById('time');
+//
+// //send form with data
+// submitForm.addEventListener('submit', (e) => {
+//     //prevent form from autosubmitting
+//     e.preventDefault();
+//
+//     //execute function to fetch data and parse the values as arguments
+//     fetchRecipeData(
+//         ingredients.value,
+//         mealType.value,
+//         cuisine.value,
+//         diet.value,
+//         time.value,
+//
+//     );
+// });
+
+import fetchRecipeData from "./functions/fetchRecipeData";
+
+//Reference to form submit
+const submitForm = document.getElementById('onSubmit');
+
+//reference to input fields
+const ingredients = document.getElementById('ingredients');
+const mealType = document.getElementById('mealType');
+const cuisine = document.getElementById('cuisine');
+const diet = document.getElementById('diet');
+const time = document.getElementById('time')
+
+//send form with data
+submitForm.addEventListener('submit', (e) => {
+        //prevent form from auto-submitting
+        e.preventDefault(); //essentieel
+
+        //execute function to fetch data and parse our values as arguements;
+        fetchRecipeData(
+            ingredients.value,
+            mealType.value,
+            cuisine.value,
+            diet.value,
+            time.value,
+        );
+    }
+);
